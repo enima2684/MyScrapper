c036dd730ae100d835807678b37c389f5382e077
amine <amine.bouamama@gmail.com>
2021-07-12 08:20:00 +0200

    update

--- writing_data_on_csv.py
+++ writing_data_on_csv.py
@@ -3,17 +3,17 @@ import csv
 
 
 def write_on_csv(data):
+    file_exists = os.path.isfile('googleresults.csv')
 
-	file_exists = os.path.isfile('googleresults.csv')
+    with open('googleresults.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Title', 'Links']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if not file_exists:
+            writer.writeheader()
 
-	with open ('googleresults.csv', 'a',newline='') as csvfile:
-		fieldnames = ['Title', 'Links']
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-		if not file_exists:
-			writer.writeheader()
-		
-		writer.writerow(data)
-		print ('wrote on to the file')
+        writer.writerow(data)
+        print('wrote on to the file')
+
 
 data = {'Title': 'Sourcebits mobile app development and design', 'Links': 'sourcebits.com/'}
-print (write_on_csv(data))
+print(write_on_csv(data))
